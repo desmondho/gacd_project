5142dad1fdd6502bba2c08132b998e246f793897
Desmond Ho <desymho@gmail.com>
2015-10-25 15:51:13 -0500

    run_analysis.R and tidydata.txt

--- run_analysis.R
+++ run_analysis.R
@@ -1,23 +1,66 @@
-#get the list of files
+# get the list of files
 path_rf <- file.path("../data" , "UCI HAR Dataset")
 files<-list.files(path_rf, recursive=TRUE)
 files
-#Read data from the files into the variables
+# Read data from the files into the variables
 #
-#Read Activity
+# Read Activity
 dataActivityTest  <- read.table(file.path(path_rf, "test" , "Y_test.txt" ),header = FALSE)
 dataActivityTrain <- read.table(file.path(path_rf, "train", "Y_train.txt"),header = FALSE)
-#Read Subject
+# Read Subject
 dataSubjectTrain <- read.table(file.path(path_rf, "train", "subject_train.txt"),header = FALSE)
 dataSubjectTest  <- read.table(file.path(path_rf, "test" , "subject_test.txt"),header = FALSE)
-#Read Features
+# Read Features
 dataFeaturesTest  <- read.table(file.path(path_rf, "test" , "X_test.txt" ),header = FALSE)
 dataFeaturesTrain <- read.table(file.path(path_rf, "train", "X_train.txt"),header = FALSE)
-#View properties of variables
-#OPTIONAL
-#str(dataActivityTest)
-#str(dataActivityTrain)
-#str(dataSubjectTrain)
-#str(dataSubjectTest)
-#str(dataFeaturesTest)
-#str(dataFeaturesTrain)
+# View properties of variables
+# OPTIONAL
+# str(dataActivityTest)
+# str(dataActivityTrain)
+# str(dataSubjectTrain)
+# str(dataSubjectTest)
+# str(dataFeaturesTest)
+# str(dataFeaturesTrain)
+
+# Concatenate the data tables by rows
+dataSubject <- rbind(dataSubjectTrain, dataSubjectTest)
+dataActivity<- rbind(dataActivityTrain, dataActivityTest)
+dataFeatures<- rbind(dataFeaturesTrain, dataFeaturesTest)
+# Set names to variables
+names(dataSubject)<-c("subject")
+names(dataActivity)<- c("activity")
+dataFeaturesNames <- read.table(file.path(path_rf, "features.txt"),head=FALSE)
+names(dataFeatures)<- dataFeaturesNames$V2
+# Merge columns to get the data 
+dataCombine <- cbind(dataSubject, dataActivity)
+Data <- cbind(dataFeatures, dataCombine)
+# Extracts only the measurements on the mean and standard deviation for each measurement
+subdataFeaturesNames<-dataFeaturesNames$V2[grep("mean\\(\\)|std\\(\\)", dataFeaturesNames$V2)]
+selectedNames<-c(as.character(subdataFeaturesNames), "subject", "activity" )
+Data<-subset(Data,select=selectedNames)
+str(Data)
+activityLabels <- read.table(file.path(path_rf, "activity_labels.txt"),header = FALSE)
+# Check data activity
+# head(Data$activity,30)
+
+# In the former part, variables activity and subject and names of the activities have been labelled using descriptive names.In this part, Names of Feteatures will labelled using descriptive variable names.
+# prefix t is replaced by time
+# Acc is replaced by Accelerometer
+# Gyro is replaced by Gyroscope
+# prefix f is replaced by frequency
+# Mag is replaced by Magnitude
+# BodyBody is replaced by Body
+names(Data)<-gsub("^t", "time", names(Data))
+names(Data)<-gsub("^f", "frequency", names(Data))
+names(Data)<-gsub("Acc", "Accelerometer", names(Data))
+names(Data)<-gsub("Gyro", "Gyroscope", names(Data))
+names(Data)<-gsub("Mag", "Magnitude", names(Data))
+names(Data)<-gsub("BodyBody", "Body", names(Data))
+# Optional check
+# names(Data)
+# Tidy data output
+library(plyr);
+Data2<-aggregate(. ~subject + activity, Data, mean)
+Data2<-Data2[order(Data2$subject,Data2$activity),]
+write.table(Data2, file = "tidydata.txt",row.name=FALSE)
+
