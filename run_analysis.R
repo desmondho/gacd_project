a558a38782d74c52939e34a69ad75a2fa2a2b046
Desmond Ho <desymho@gmail.com>
2015-10-25 16:41:43 -0500

    variables issue fixed, rerun code

--- run_analysis.R
+++ run_analysis.R
@@ -1,46 +1,53 @@
 # get the list of files
-path_rf <- file.path("../data" , "UCI HAR Dataset")
-files<-list.files(path_rf, recursive=TRUE)
+dataPath <- file.path("../data" , "UCI HAR Dataset")
+files<-list.files(dataPath, recursive=TRUE)
 files
 # Read data from the files into the variables
 #
 # Read Activity
-dataActivityTest  <- read.table(file.path(path_rf, "test" , "Y_test.txt" ),header = FALSE)
-dataActivityTrain <- read.table(file.path(path_rf, "train", "Y_train.txt"),header = FALSE)
+activityTest  <- read.table(file.path(dataPath, "test" , "Y_test.txt" ),header = FALSE)
+activityTrain <- read.table(file.path(dataPath, "train", "Y_train.txt"),header = FALSE)
 # Read Subject
-dataSubjectTrain <- read.table(file.path(path_rf, "train", "subject_train.txt"),header = FALSE)
-dataSubjectTest  <- read.table(file.path(path_rf, "test" , "subject_test.txt"),header = FALSE)
+subjectTrain <- read.table(file.path(dataPath, "train", "subject_train.txt"),header = FALSE)
+subjectTest  <- read.table(file.path(dataPath, "test" , "subject_test.txt"),header = FALSE)
 # Read Features
-dataFeaturesTest  <- read.table(file.path(path_rf, "test" , "X_test.txt" ),header = FALSE)
-dataFeaturesTrain <- read.table(file.path(path_rf, "train", "X_train.txt"),header = FALSE)
+featuresTest  <- read.table(file.path(dataPath, "test" , "X_test.txt" ),header = FALSE)
+featuresTrain <- read.table(file.path(dataPath, "train", "X_train.txt"),header = FALSE)
+# Read Features
+featuresNames <- read.table(file.path(dataPath, "features.txt"),head=FALSE)
+# Read Activity Labels
+activityLabels <- read.table(file.path(dataPath, "activity_labels.txt"),header = FALSE)
+
 # View properties of variables
 # OPTIONAL
-# str(dataActivityTest)
-# str(dataActivityTrain)
-# str(dataSubjectTrain)
-# str(dataSubjectTest)
-# str(dataFeaturesTest)
-# str(dataFeaturesTrain)
+# str(activityTest)
+# str(activityTrain)
+# str(subjectTrain)
+# str(subjectTest)
+# str(featuresTest)
+# str(featuresTrain)
 
 # Concatenate the data tables by rows
-dataSubject <- rbind(dataSubjectTrain, dataSubjectTest)
-dataActivity<- rbind(dataActivityTrain, dataActivityTest)
-dataFeatures<- rbind(dataFeaturesTrain, dataFeaturesTest)
+subject <- rbind(subjectTrain, subjectTest)
+activity<- rbind(activityTrain, activityTest)
+features<- rbind(featuresTrain, featuresTest)
 # Set names to variables
-names(dataSubject)<-c("subject")
-names(dataActivity)<- c("activity")
-dataFeaturesNames <- read.table(file.path(path_rf, "features.txt"),head=FALSE)
-names(dataFeatures)<- dataFeaturesNames$V2
+names(subject)<-c("subject")
+names(activity)<- c("activity")
+
+names(features)<- featuresNames$V2
 # Merge columns to get the data 
-dataCombine <- cbind(dataSubject, dataActivity)
-Data <- cbind(dataFeatures, dataCombine)
+dataCombine <- cbind(subject, activity)
+Data <- cbind(features, dataCombine)
 # Extracts only the measurements on the mean and standard deviation for each measurement
-subdataFeaturesNames<-dataFeaturesNames$V2[grep("mean\\(\\)|std\\(\\)", dataFeaturesNames$V2)]
-selectedNames<-c(as.character(subdataFeaturesNames), "subject", "activity" )
+subfeaturesNames<-featuresNames$V2[grep("mean\\(\\)|std\\(\\)", featuresNames$V2)]
+selectedNames<-c(as.character(subfeaturesNames), "subject", "activity" )
 Data<-subset(Data,select=selectedNames)
-str(Data)
-activityLabels <- read.table(file.path(path_rf, "activity_labels.txt"),header = FALSE)
+# str(Data)
+
 # Check data activity
+Data$activity<-factor(Data$activity);
+Data$activity<- factor(Data$activity,labels=as.character(activityLabels$V2))
 # head(Data$activity,30)
 
 # In the former part, variables activity and subject and names of the activities have been labelled using descriptive names.In this part, Names of Feteatures will labelled using descriptive variable names.
